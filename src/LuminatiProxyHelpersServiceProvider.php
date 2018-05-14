5b9aec1a6b01c672afabc49146a07997b9533e4c
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-14 15:58:36 +0300

    Added Luminati Proxy Helpers service provider with package binding.

---
+++ src/LuminatiProxyHelpersServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace JorgenVatle\LuminatiProxyHelpers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LuminatiProxyHelpersServiceProvider extends ServiceProvider {
+
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(LuminatiProxyHelpers::class, function ($app) {
+            return new LuminatiProxyHelpers();
+        });
+    }
+
+}
