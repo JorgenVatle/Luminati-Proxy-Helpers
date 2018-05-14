b43c9798eb5f99e4515cf73542b922ef157d1b12
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-14 16:32:00 +0300

    Added Proxy class with string parser.

---
+++ src/Types/Proxy.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace JorgenVatle\LuminatiProxyHelpers\Types;
+
+class Proxy {
+
+    /**
+     * Proxy host.
+     *
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * Proxy port.
+     *
+     * @var int
+     */
+    protected $port;
+
+    /**
+     * Proxy constructor.
+     *
+     * @param string $proxy
+     */
+    function __construct(string $proxy) {
+        $this->buildFromString($proxy);
+    }
+
+    /**
+     * Sanitizes, converts and assigns the given proxy to the Proxy instance.
+     *
+     * @param $proxy
+     */
+    protected function buildFromString($proxy) {
+        $cleanedProxy = preg_replace('/([a-z]+:\/\/)/i', '', $proxy);
+
+        $parts = explode(':', $cleanedProxy, 2);
+
+        $this->host = $parts[0];
+        $this->port = $parts[1];
+    }
+
+}
