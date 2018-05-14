e0d1c66fa790bbd294ff53b67a9f506781051420
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-14 15:41:12 +0300

    added LuminatiProxyHelpers base class properties.

---
+++ src/LuminatiProxyHelpers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace JorgenVatle\LuminatiProxyHelpers;
+
+class LuminatiProxyHelpers {
+
+    /**
+     * Proxy port as defined in the LPM General Settings tab.
+     *
+     * @var int
+     */
+    protected $httpPortStart = 24000;
+
+    /**
+     * SOCKS 5 Port as defined in the LPM General Settings tab.
+     *
+     * @var int
+     */
+    protected $socksPortStart = 1500;
+
+}
