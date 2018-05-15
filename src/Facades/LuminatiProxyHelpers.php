780ca89058d996b2c97e7ec04aa56c968e78bbe8
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-16 01:26:44 +0300

    Added IDE helpers to LuminatiProxyHelpers facade.

--- src/Facades/LuminatiProxyHelpers.php
+++ src/Facades/LuminatiProxyHelpers.php
@@ -4,6 +4,17 @@ namespace JorgenVatle\LuminatiProxyHelpers\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * LuminatiProxyHelpers Facade
+ *
+ *
+ * @method static int getSocksPort(int $httpPort)
+ * @method static int getHttpPort(int $socksPort)
+ * @method static \JorgenVatle\LuminatiProxyHelpers\Types\SocksProxy getSocks(string $httpProxyString)
+ * @method static \JorgenVatle\LuminatiProxyHelpers\Types\HttpProxy getHttp(string $socksProxyString)
+ *
+ * @package JorgenVatle\LuminatiProxyHelpers\Facades
+ */
 class LuminatiProxyHelpers extends Facade {
 
     /**
