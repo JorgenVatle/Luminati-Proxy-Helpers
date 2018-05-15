acfbecc64c40a6613e56d4036d3cd54a4052cd33
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-15 19:42:50 +0300

    Asserting that the luminati proxy helpers can get a SOCKS proxy from a HTTP proxy.

---
+++ tests/unit/ProxyHelpersTest.php
@@ -0,0 +1,17 @@
+<?php namespace JorgenVatle\LuminatiProxyHelpers\Tests\Unit;
+
+use JorgenVatle\LuminatiProxyHelpers\Facades\LuminatiProxyHelpers;
+use JorgenVatle\LuminatiProxyHelpers\Tests\TestCase;
+
+class ProxyHelpersTest extends TestCase {
+
+    /** @test */
+    public function it_can_resolves_socks_ports_from_http_proxies() {
+        $proxy = LuminatiProxyHelpers::getSocks('127.0.0.1:22000');
+
+        $this->assertEquals('socks', $proxy->getType());
+        $this->assertEquals(1500, $proxy->getPort());
+        $this->assertEquals('127.0.0.1', $proxy->getHost());
+    }
+
+}
