571a456ea60b4f3fe47d8c0e56e2de5aec139390
Jørgen Vatle <Jorgen@WcAServices.net>
2018-05-15 19:30:41 +0300

    Added TestCase aliases.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,13 +5,25 @@ namespace JorgenVatle\LuminatiProxyHelpers\Tests;
 class TestCase extends \Orchestra\Testbench\TestCase {
 
     /**
-     * Package service provider
+     * Package service provider.
      *
      * @param \Illuminate\Foundation\Application $app
      * @return array
      */
     protected function getPackageProviders($app) {
         return ['JorgenVatle\LuminatiProxyHelpers'];
+    }
+
+    /**
+     * Package aliases.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageAliases($app) {
+        return [
+            'LuminatiProxyHelpers' => 'JorgenVatle\LuminatiProxyHelpers\Facades\LuminatiProxyHelpers',
+        ];
     }
 
 }
