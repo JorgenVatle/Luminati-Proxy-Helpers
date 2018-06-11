8e4c5db414b2b1f6ef6801a300c7e4ff6f32706f
Jørgen Vatle <Jorgen@WcAServices.net>
2018-06-11 14:25:29 +0300

    Added configuration path and file publication to service provider.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,17 +4,35 @@ namespace JorgenVatle\LuminatiProxyHelpers;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider {
 
+    /**
+     * Package configuration file path.
+     *
+     * @var string
+     */
+    protected $config = __DIR__.'/../config/proxyhelpers.php';
+
+    /**
+     * Package bootstrapper.
+     */
+    public function boot() {
+
+        $this->publishes([
+            $this->config => config_path('proxyhelpers.php'),
+        ], 'luminati-proxy-helpers');
+
+    }
 
     /**
      * Register bindings in the container.
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
+
         $this->app->singleton('luminati.proxy.helpers', function ($app) {
             return new LuminatiProxyHelpers(22000, 1500);
         });
+
     }
 
 }
